c2bf9c5dcbaa6acb9efab48dc4ababbd504b91e6
Laura <laurajadefogarty@gmail.com>
2019-04-01 16:07:08 +0100

    amend create_user_table migration to include not nullable

--- db/migrations/20190401143808_create_user_table.js
+++ db/migrations/20190401143808_create_user_table.js
@@ -1,7 +1,7 @@
 
 exports.up = function(knex, Promise) {
   return knex.schema.createTable('users', (usersTable) => {
-    usersTable.string('username').unique().primary();
+    usersTable.string('username').unique().primary().notNullable();
     usersTable.string('avatar_url');
     usersTable.string('name');
   });
