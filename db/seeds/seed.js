fd08a0293e1ac196e5cd1084a2b2346c3061b16a
Laura <laurajadefogarty@gmail.com>
2019-04-02 14:10:02 +0100

    add topic seeding

--- db/seeds/seed.js
+++ db/seeds/seed.js
@@ -1,10 +1,20 @@
-// const {  } = require('../data');
+const { articleData, commentData, topicData, userData } = require('../data');
 
 exports.seed = (knex, Promise) => {
   return knex.migrate
     .rollback()
     .then(() => knex.migrate.latest())
+    .then(() => knex('topics').insert(topicData).returning('*')).then((topicRows) => console.log(topicRows))
     .then(() => {
-      // insert data
+     return knex('users').insert(userData).returning('*')
+    // .then(() => {
     });
 };
+
+// .then(() => { return knex('articles').insert(articleData).returning('*') })
+// const formatTime 
+// need util for changing timestamp into a date
+// .then(() => { return knex('comments').insert(commentData).returning('*' ) })
+// need util for changing timestamp into a date
+// need util for author being username (created by in data file)
+// n.b.  belongs_to is article title
