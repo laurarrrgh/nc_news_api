18de7e411dd74f4aab286356195af689af670cea
Laura <laurajadefogarty@gmail.com>
2019-04-03 11:15:01 +0100

    amend spec file console logs to make testing more readable

--- db/seeds/seed.js
+++ db/seeds/seed.js
@@ -6,23 +6,23 @@ exports.seed = (knex, Promise) => {
     .rollback()
     .then(() => knex.migrate.latest())
     .then(() => knex('topics').insert(topicData).returning('*'))
-    .then((topicRows) => {
-      console.log(topicRows) 
+    .then((topics) => {
+      console.log(`${topics.length} topics inserted`) 
       return knex('users').insert(userData).returning('*')
     })
-      .then((userRows) => {
-      console.log(userRows)
+      .then((users) => {
+      console.log(`${users.length} users inserted`)
       const formatTime = createNewTimeFormat(articleData);
       return knex('articles').insert(formatTime).returning('*')
     })
-  .then((articleRows) => {
-     console.log(articleRows) 
+  .then((articles) => {
+      console.log(`${articles.length} articles inserted`) 
      // format comment data
-     // return knex('comments').insert(constAbove).returning('*')
+     // return knex('comments').insert(commentData).returning('*')
   })
 };
 
 
 // need util for changing timestamp into a date
 // need util for author being username (created by in data file)
-// n.b.  belongs_to is article title - need to create reference object
+// n.b.  belongs_to is article title from comment to article - need to create reference object super complicated - DOUBLE CHECK LAURA
